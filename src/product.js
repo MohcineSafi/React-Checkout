604dcbf30b28ddaf6094b3db62f026e1a9629582
MohcineSafi <safi.mouhcine02@gmail.com>
2024-10-15 12:43:44 +0100

    feat: Add product images and data

---
+++ src/product.js
@@ -0,0 +1,49 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro(M1)",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Canon",
+    price: 699.0,
+    productImage: product3,
+  },
+
+  {
+    id: 4,
+    productName: "LED Light",
+    price: 19.99,
+    productImage: product5,
+  },
+
+  {
+    id: 5,
+    productName: "AUTO SERVICE SHIRT",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 6,
+    productName: "UNSTRUCTURED HAT",
+    price: 40.0,
+    productImage: product8,
+  },
+];
